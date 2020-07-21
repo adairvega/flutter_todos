e92e97a3e40e827a6a849768744d3fd3e58694f4
adairvega <adairgonzalez@utez.edu.mx>
2020-07-21 09:06:58 +0200

    Todo list following the BLoC flutter documentation.

---
+++ lib/blocs/blocs.dart
@@ -0,0 +1,7 @@
+export './filtered_todos/filtered_todos.dart';
+export './stats/stats_bloc.dart';
+export './stats/stats_event.dart';
+export './stats/stats_state.dart';
+export './tab/tab.dart';
+export './todos/todos.dart';
+export './simple_bloc_observer.dart';
